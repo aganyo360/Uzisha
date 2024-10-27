b63bcfdbba853dbe00baf84cb0fb7b6da76762f4
aganyo360 <wilsonaganyo@gmail.com>
2024-10-27 22:53:23 +0300

    updated the Image component

--- App.js
+++ App.js
@@ -9,6 +9,23 @@ export default function App() {
       <Text>About</Text>
       {/* <StatusBar style="auto" /> */}
       <Text numberOfLines="{10}">This is a sample explanation to show how the numberoflines prop on react affects the number of lines</Text>
+
+      <Image
+        source={require('./assets/splash.png')}
+        // this is only for a local image
+        />
+        <Image
+        //  for the network image or images with links we use the following\
+        source={{
+          width:300,
+          height:200,
+          uri: 'https://example.com/image.jpg',
+        }}
+        //  resizeMode="contain" // or "cover" or "stretch"
+        //  style={{ width: 200, height: 200 }} // adjust the size as needed
+        //  blurRadius={10} // for blur effect
+        />
+
     </SafeAreaView>
   );
 }
